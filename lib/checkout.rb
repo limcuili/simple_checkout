7dc63f5b75454d91cbd3bf2540f81fe86c1b622f
limcuili <limcuili@hotmail.com>
2018-07-19 20:00:56 +0100

    Add inventory hash

---
+++ lib/checkout.rb
@@ -0,0 +1,12 @@
+class Checkout
+
+  INVENTORY = { bread: 0.89, milk: 1.25, tea: 2, coffee: 2.5,
+    cereal: 4.20, pasta: 5.50, apples: 2.85, bananas: 3, lemons: 2,
+    chips: 0.95, chocolates: 13.50, wine: 12.75, beer: 8 }
+
+
+  def view_price(item)
+  end
+
+
+end
